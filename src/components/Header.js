7eb11a8a07116f2b5155a30f9a1f8ee15d2c27bd
Sareen Ahamed <sareenahamed40@gmail.com>
2025-04-16 11:10:36 +0530

    Header initialized

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header className="bg-white shadow-lg">
         <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-            <h1 className="text-2xl">Blog</h1>
+            <h1 className="text-2xl text-blue-500">Blog</h1>
             <nav className="space-x-4">
                 <a href="#" className="text-blue-500">Home</a>
                 <a href="#" className="text-blue-500">About</a>
